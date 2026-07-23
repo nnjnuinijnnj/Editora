c544e4d0e68f6e9041623337f177d4cb2cfa0db6
nnjnuinijnnj <abzartajhiz@gmail.com>
2026-07-23 21:51:39 +0330

    Create photo-editor.js

---
+++ photo-editor.js
@@ -0,0 +1,20 @@
+const imageInput = document.getElementById("imageInput");
+const preview = document.getElementById("preview");
+
+imageInput.addEventListener("change", function () {
+
+    const file = this.files[0];
+
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+
+        preview.src = e.target.result;
+
+    };
+
+    reader.readAsDataURL(file);
+
+});
