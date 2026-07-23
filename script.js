c4432ab4235acce690aa69bcbd75b1e1a6eb0d00
nnjnuinijnnj <abzartajhiz@gmail.com>
2026-07-23 21:48:56 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+// Welcome
+console.log("Welcome to Editora!");
+
+// Buttons
+const photoBtn = document.getElementById("photoBtn");
+const videoBtn = document.getElementById("videoBtn");
+const projectBtn = document.getElementById("projectBtn");
+const templateBtn = document.getElementById("templateBtn");
+const settingBtn = document.getElementById("settingBtn");
+
+// Photo Editor
+photoBtn.addEventListener("click", () => {
+    alert("🚧 Photo Editor - Coming Soon");
+});
+
+// Video Editor
+videoBtn.addEventListener("click", () => {
+    alert("🚧 Video Editor - Coming Soon");
+});
+
+// My Projects
+projectBtn.addEventListener("click", () => {
+    alert("🚧 My Projects - Coming Soon");
+});
+
+// Templates
+templateBtn.addEventListener("click", () => {
+    alert("🚧 Templates - Coming Soon");
+});
+
+// Settings
+settingBtn.addEventListener("click", () => {
+    alert("🚧 Settings - Coming Soon");
+});
